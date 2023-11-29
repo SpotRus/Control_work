49d62065121421ce55931185b6f96b17d6702c6c
SpotRus <spotik56rus@gmail.com>
2023-11-29 13:22:51 +0300

    Added a method PrintArray

--- Work01/Program.cs
+++ Work01/Program.cs
@@ -1,17 +1,29 @@
-﻿string[] GetUserInput()
+﻿using System.Data;
+
+string[] GetUserInput()
 {
-    Console.WriteLine("Укажите размер массива: ");
+    Console.Write("Укажите размер массива: ");
     int size = Convert.ToInt32(Console.ReadLine());
     string[] arr = new string[size];
 
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.WriteLine($"Укажите {i + 1} элемент массива: ");
+        Console.Write($"Укажите {i + 1} элемент массива: ");
         arr[i] = Console.ReadLine();
     }
     return arr;
 }
 
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr.Last()}]");
+}
 
 string[] array = GetUserInput();
+PrintArray(array);
 
