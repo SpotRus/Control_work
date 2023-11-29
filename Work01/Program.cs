29cea67af627107cca41eb23ea12cb1f9a1de0d5
SpotRus <spotik56rus@gmail.com>
2023-11-29 13:01:26 +0300

    Added a method GetUserInput

---
+++ Work01/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] GetUserInput()
+{
+    Console.WriteLine("Укажите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] arr = new string[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Укажите {i + 1} элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
+string[] array = GetUserInput();
+
